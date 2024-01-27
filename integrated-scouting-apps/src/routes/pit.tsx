2f734e501d3dd63b5da29aed8706a624e8ca5ba8
rkang1357 <rkang1357@gmail.com>
2024-01-27 10:59:10 -0800

    completed front end of pit scout

--- integrated-scouting-apps/src/routes/pit.tsx
+++ integrated-scouting-apps/src/routes/pit.tsx
@@ -2,13 +2,18 @@ import '../public/stylesheets/style.css';
 import logo from '../public/images/logo.png';
 import { Checkbox, Flex, Form, Input, InputNumber, Select, Tabs, TabsProps, Upload, message } from 'antd';
 import { useEffect, useState } from 'react';
+<<<<<<< Updated upstream
 import TextArea from 'antd/es/input/TextArea';
 import { RcFile, UploadChangeParam, UploadFile, UploadProps } from 'antd/es/upload/interface';
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+=======
+import {Button} from 'antd';
+>>>>>>> Stashed changes
 
 function PitScout(props: any) {
   const [form] = Form.useForm();
   useEffect(() => document.title = props.title, [props.title]);
+<<<<<<< Updated upstream
 
   type PitType = {
     initials?: string,
@@ -249,6 +254,25 @@ function PitScout(props: any) {
     }
   ];
   return (
+=======
+  
+  const intakeCap = [
+    { label: "Source", value: "source" },
+    { label: "Ground", value: "ground" },
+    { label: "Both", value: 'both' },
+  ];
+  const shootingCap = [
+    { label: "Speaker", value: "speaker" },
+    { label: "Amp", value: "amp" },
+    { label: "Both", value: 'both' },
+  ];
+  const climbingCap = [
+    { label: "Solo Climb", value: "solo_climb" },
+    { label: "Harmonize", value: "harmonize" },
+    { label: "Triple Climb", value: 'triple_climb' },
+  ];
+  return(
+>>>>>>> Stashed changes
     <body>
       <div className='banner'>
         <header>
@@ -256,6 +280,7 @@ function PitScout(props: any) {
           <h1>Pit Scout</h1>
         </header>
       </div>
+<<<<<<< Updated upstream
       <Form
         form={form}
         onFinish={async event => {
@@ -277,6 +302,144 @@ function PitScout(props: any) {
       >
         <Tabs defaultActiveKey="1" items={items} />
       </Form>
+=======
+      
+      {/* robot pictures */}
+      <div style={{marginBottom:"20%"}}>
+        <h1 className='pitBody' style={{fontSize:'220%'}}>Robot Pictures</h1>
+        <Button style={{float:'left'}} className='pitButton'>Upload</Button>
+        <Button style={{float:'right'}}className='pitButton'>Upload</Button>
+        
+      </div>
+
+      {/* how many events */}
+      <div>
+        <h1 className='pitBody'>How many events have to competed in?</h1>
+        <Form.Item name="eventNum" rules={[{required: true, message: 'Please input the robot position!' }]}>
+          <InputNumber className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* team # */}
+      <div>
+        <h1 className='pitBody'>Team #</h1>
+        <Form.Item>
+          <InputNumber className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* drive train type */}
+      <div>
+        <h1 className='pitBody'>Drive Train Type</h1>
+        <Form.Item>
+          <Input className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* robot tuype */}
+      <div>
+        <h1 className='pitBody'>Robot Type</h1>
+        <Form.Item>
+          <Input className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* # of Motors and what type/*/}
+      <div>
+        <h1 className='pitBody'># of motors and what type?</h1>
+        <Form.Item>
+          <label>
+            <textarea className="pitComment" name="eventNum" rows={3}/>
+          </label>
+        </Form.Item>
+      </div>
+
+      {/* intake capability/*/}
+      <div>
+        <h1 className='pitBody'>Intake capability</h1>
+        <Form.Item name="intakeCap" rules={[{ required: true, message: 'Please input the intake capability!' }]}>
+          <Select placeholder='Source' options={intakeCap} className="input"/>
+        </Form.Item>
+      </div>
+
+      {/* shooting capability/*/}
+      <div>
+        <h1 className='pitBody' style={{marginTop:"7%"}}>Shooting capability</h1>
+        <Form.Item name="shootingCap" rules={[{ required: true, message: 'Please input the shooting capability!' }]}>
+          <Select placeholder='Speaker' options={shootingCap} className="input"/>
+        </Form.Item>
+      </div>
+
+      {/* go under stage */}
+      <div>
+        <h1 className='pitBody' style={{marginTop:"7%"}}>Under Stage</h1>
+        <Form.Item name="under">
+          <Checkbox className='input_checkbox'/>
+        </Form.Item>
+      </div>
+
+      {/* climbing capabilities */}
+      <div>
+        <h1 className='pitBody' style={{marginTop:"7%"}}>Climbing capability</h1>
+        <Form.Item name="climbingCap" rules={[{ required: true, message: 'Please input the climbing capability!' }]}>
+          <Select placeholder='Solo Climb' options={climbingCap} className="input"/>
+        </Form.Item>
+      </div>
+
+      {/* How does robot trap? */}
+      <div>
+        <h1 className='pitBody' style={{marginTop:"7%"}}>How does robot trap?</h1>
+        <Form.Item>
+          <label>
+            <textarea className="pitComment" name="eventNum" rows={3}/>
+          </label>
+        </Form.Item>
+      </div>
+
+      {/* pit organization */}
+      <div>
+        <h1 className='pitBody'>Pit organization</h1>
+        <Form.Item>
+         <InputNumber className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* Team safety */}
+      <div>
+        <h1 className='pitBody'>Team safety</h1>
+        <Form.Item>
+          <InputNumber className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* Team workmenship# */}
+      <div>
+        <h1 className='pitBody'>Team workmenship</h1>
+        <Form.Item>
+          <InputNumber className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* Gracious professionalism */}
+      <div>
+        <h1 className='pitBody'>Gracious professionalism</h1>
+        <Form.Item>
+          <InputNumber className="pitinput"/>
+        </Form.Item>
+      </div>
+
+      {/* submit */}
+      <div>
+          <Button style={{marginLeft: '25%', marginTop: '10%'}} className='pitButton'>Submit</Button>
+      </div>
+
+      
+
+      
+
+      
+
+>>>>>>> Stashed changes
     </body>
   );
 }
