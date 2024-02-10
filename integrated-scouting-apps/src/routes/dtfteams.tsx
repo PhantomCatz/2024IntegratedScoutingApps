4156b652d7bd3aa61a845cd100ee5ebbbdcb9b54
Juwon Ha <hajuwon@gmail.com>
2024-02-09 17:04:03 -0800

    DTFTF

--- integrated-scouting-apps/src/routes/dtfteams.tsx
+++ integrated-scouting-apps/src/routes/dtfteams.tsx
@@ -1,9 +1,10 @@
 import '../public/stylesheets/dtf.css';
 import logo from '../public/images/logo.png';
 import back from '../public/images/back.png';
-import { useEffect, useState } from 'react';
-import { Tabs, TabsProps } from 'antd';
+import { useEffect, useState, useRef } from 'react';
+import { Tabs, TabsProps, Input, Form, Select, Checkbox, InputNumber, Flex, Button, Statistic } from 'antd';
 import { useParams } from 'react-router-dom';
+import { Row, Col } from 'antd';
 
 function DTFTeams(props: any) {
   useEffect(() => document.title = props.title, [props.title]);
@@ -15,6 +16,14 @@ function DTFTeams(props: any) {
   let team2_number = '2637';//null protection
   let team3_number = '2637';//null protection
 
+  function generateSquare(width: number, height: number, x: number, y: number) { //width: width of square, height: height of square, x: x-coordinte of square, y: y-coordinate of square, fyi: there are all percent input
+    return (
+      <svg width={width + x + '%'} height={height + y + '%'}>
+        <rect x={x + '%'} y={y + '%'} width={width + '%'} height={height + '%'} fill="none" stroke="white" strokeWidth="4" />
+      </svg>
+    );
+  }
+
   if(team_number){
     const teams = team_number.split(',');
     console.log(teams);
@@ -24,13 +33,13 @@ function DTFTeams(props: any) {
     if(teams[1]) {
       team2_number = teams[1];
     } else {
-      team2_number = 'OwO';
+      team2_number = ' ';
     }
 
     if(teams[2]) {
       team3_number = teams[2];
     } else {
-      team3_number = 'OwO';
+      team3_number = ' ';
     }
   }
   
@@ -39,24 +48,95 @@ function DTFTeams(props: any) {
   function Summary() {
     return (
       <div>
-        <h2>working</h2>
+        <h2 style={{textAlign:'center'}}>Allience Average Score</h2>
+          <InputNumber controls placeholder='team 1' min={1} className="input"/>
+        <h2 style={{textAlign: 'center'}}>Auton Path</h2>
+        <div style={{textAlign: 'center'}}>
+          <h2 style={{display: 'inline'}}>Team1</h2> 
+          <h2 style={{display: 'inline', marginLeft: '7%'}}>Team2</h2> 
+          <h2 style={{display: 'inline', marginLeft: '7%'}}>Team3</h2>
+        </div>
+        <div style={{textAlign: 'center'}}>
+        <img src={logo} style={{ height: 256 + 'px' }} alt=''/>
+        <img src={logo} style={{ height: 256 + 'px', marginLeft: '2%' }} alt=''/>
+        <img src={logo} style={{ height: 256 + 'px', marginLeft: '2%' }} alt=''/>
+        </div>
+        
+
+        <h2 style={{textAlign: 'center'}}>Intake Source</h2>
+
+        <div style={{textAlign: 'center'}}>
+          <h2 style={{display: 'inline'}}>Team1</h2> 
+          <h2 style={{display: 'inline', marginLeft: '15%'}}>Team2</h2> 
+          <h2 style={{display: 'inline', marginLeft: '15%'}}>Team3</h2>
+        </div>
+        <div style={{display: 'inline'}}>
+        <Checkbox className='checkboxRedFixed'/>
+        <Checkbox className='checkboxRedFixed'/>
+        </div>
+        <div style={{display: 'inline', marginLeft: '4%'}}>
+        <Checkbox className='checkboxRedFixed'/>
+        <Checkbox className='checkboxRedFixed'/>
+        </div>
+        <div style={{display: 'inline', marginLeft: '4%'}}>
+        <Checkbox className='checkboxRedFixed'/>
+        <Checkbox className='checkboxRedFixed'/>
+        </div>
+        <h2 style={{textAlign: 'center'}}>Traversed Stage</h2>
+        <div style={{textAlign: 'center'}}>
+          <h2 style={{display: 'inline'}}>Team1</h2> 
+          <h2 style={{display: 'inline', marginLeft: '15%'}}>Team2</h2> 
+          <h2 style={{display: 'inline', marginLeft: '15%'}}>Team3</h2>
+        </div>
+        <div style={{display: 'inline'}}>
+        <Checkbox className='checkboxRedFixed'/>
+        <Checkbox className='checkboxRedFixed'/>
+        </div>
+        <div style={{display: 'inline', marginLeft: '4%'}}>
+        <Checkbox className='checkboxRedFixed'/>
+        <Checkbox className='checkboxRedFixed'/>
+        </div>
+        <div style={{display: 'inline', marginLeft: '4%'}}>
+        <Checkbox className='checkboxRedFixed'/>
+        <Checkbox className='checkboxRedFixed'/>
+        </div>
+        <h2 style={{textAlign: 'center'}}>Driver Skill</h2>
+        <div style={{textAlign: 'center'}}>
+          <h2 style={{display: 'inline'}}>Team1</h2> 
+          <h2 style={{display: 'inline', marginLeft: '15%'}}>Team2</h2> 
+          <h2 style={{display: 'inline', marginLeft: '15%'}}>Team3</h2>
+        </div>
+
+        <div style={{display: 'inline'}}>
+        <InputNumber controls placeholder='team 1' min={1} className="inputDisplayNumber"/>
+        <InputNumber controls placeholder='team 1' min={1} className="inputDisplayNumber" style={{marginLeft: "4%"}}/>
+        <InputNumber controls placeholder='team 1' min={1} className="inputDisplayNumber" style={{marginLeft: "5%"}}/>
+        </div>
+        
+
+        <h2 style={{textAlign: 'center'}}>Graph</h2> 
+        <img src={logo} style={{ height: 100 + '%', marginLeft: 'auto', marginRight: 'auto', width: 100 + '%'}} alt=''/>
+        
       </div>
     );
   }
 
   function Team1() {
     return (
-      <div>
-        <h2>working</h2>
+      <div style={{width: '100%', height: '100%'}}>
+        {generateSquare(90, 50, 5, 50)} 
+        <svg width="95%" height="-1000000%" > 
+          <rect x="5%" y="0%" width="90%" height="100%" fill="none" stroke="white" strokeWidth="3" />
+        </svg>
       </div>
     );
   }
 
   function Team2() {
-    if(team2_number == 'OwO') {
+    if(team2_number == ' ') {
       return (
         <div>
-          <h2>working</h2>
+          <h2>tOwOt</h2>
         </div>
       );
     }
@@ -68,7 +148,7 @@ function DTFTeams(props: any) {
   }
 
   function Team3() {
-    if(team3_number == 'OwO') {
+    if(team3_number == ' ') {
       return (
         <div>
           <h2>tOwOt</h2>
