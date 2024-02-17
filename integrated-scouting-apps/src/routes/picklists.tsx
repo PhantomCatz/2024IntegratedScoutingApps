12b6a20bb47e15f3d4fa299cdefa1382f44cd222
Juwon Ha <hajuwon@gmail.com>
2024-02-17 15:47:06 -0800

    updated picklist

--- integrated-scouting-apps/src/routes/picklists.tsx
+++ integrated-scouting-apps/src/routes/picklists.tsx
@@ -2,9 +2,7 @@ import '../public/stylesheets/style.css';
 import logo from '../public/images/logo.png';
 import back from '../public/images/back.png';
 import { useEffect, useState } from 'react';
-import { Form, GetProp, Table, TableProps } from 'antd';
-import Column from 'antd/es/table/Column';
-import { useParams } from 'react-router-dom';
+import { GetProp, Table, TableColumnsType, TableProps } from 'antd';
 
 type TablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>;
 
@@ -15,37 +13,36 @@ interface TableParams {
   filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
 }
 
-interface DataType {
-  key: React.Key,
-  rank: number,
-  team_number: number,
-  match_number: number,
-  score: number,
-}
+// interface DataType {
+//   key: React.Key;
+//   rank: number;
+//   team_number: number;
+//   average_score: number;
+// }
 
-interface ExpandedDataType {
-  key: React.Key;
-  match_number: number;
-  score: number;
-}
-
+// interface DataExpand {
+//   key: React.Key;
+//   match_number: number;
+//   score: number;
+// }
 
 function Picklists(props: any) {
-  const [form] = Form.useForm();
   const eventname = process.env.REACT_APP_EVENTNAME;
   const team_number/*{ team_number }*/ = 2637;//useParams();
   const [loading, setLoading] = useState(true);
   const [fetchedData, setFetchedData] = useState<{ [x: string]: any; }[]>([]);
+  const [dataDetail, setDataDetail] = useState<{ [x: string]: any; }[]>([]);
   const [tableParams, setTableParams] = useState<TableParams>({
     pagination: {
       current: 1,
       pageSize: 100,
     },
   });
-  const rankData: any = [];
-  const teams = [];
-  let match = [1];
+
+  let match = [0];
+  let score = [0];
   let avg_score = 0;
+
   let displayData = [
     {
       
@@ -62,7 +59,7 @@ function Picklists(props: any) {
       title: 'Ranking',
       dataIndex: 'rank',
       key: 'rank',
-      width: '15%'
+      width: '15%',
     },
     {
       title: 'Team #',
@@ -70,16 +67,24 @@ function Picklists(props: any) {
       key: 'team_number',
     },
     {
-      title: 'Match #',
+      title: 'Average Score',
+      dataIndex: 'average_score',
+      key: 'average_score',
+    },
+  ];
+  
+
+  const expandColumns = [
+    {
+      title:'Match #',
       dataIndex: 'match_number',
       key: 'match_number',
-      width: '15%',
     },
     {
       title: 'Score',
       dataIndex: 'score',
-      key: 'score',
-    },
+      key: 'score' 
+    }
   ];
 
 
@@ -87,30 +92,28 @@ function Picklists(props: any) {
   useEffect(() => {
     async function fetchTeams() {
       try {
-        const response = await fetch('https://www.thebluealliance.com/api/v3/event/2023cass/rankings', {
+        const response = await fetch('https://www.thebluealliance.com/api/v3/event/'+ eventname + '/rankings', {
         method: "GET",
         headers: {
           'X-TBA-Auth-Key': '0iSKwn3ykkgDT9ToHqwBizSiiaa44pyLIK85oEdgOkzxNJS1X0vBtDFrJ24PiAWW'
         }
       });
       const data = await response.json();
-      const yo = data['rankings'];
-      console.log(yo);
+      const ls = data['rankings'];
+      console.log(ls);
       displayData = [];
-      for(var i = 0; i < yo.length; i++)
+      for(var i = 0; i < ls.length; i++)
       {
-        // fetchData(parseInt(yo[i]['team_key'].substring(3)));
-        await fetchData(2637);
-        console.log(parseInt(yo[i]['team_key'].substring(3)))
+        await fetchData(parseInt(ls[i]['team_key'].substring(3)));
+        //await fetchData(2637);
+        console.log(parseInt(ls[i]['team_key'].substring(3)))
         // console.log(avg_score, "999")
         // console.log(match, "000")
         let newData = {
-          key: yo[i]['rank'],
-          rank: yo[i]['rank'],
-          team_number: yo[i]['team_key'].substring(3),
-          match_number: match[0],//yo[i]['matches_played'],
-          score:avg_score,
-          children: [expandData][0],
+          key: ls[i]['rank'],
+          rank: ls[i]['rank'],
+          team_number: ls[i]['team_key'].substring(3),
+          average_score: avg_score,
         }
         console.log(expandData);
         console.log(newData);
@@ -118,9 +121,6 @@ function Picklists(props: any) {
       }
       console.log(displayData);
       setFetchedData(displayData);
-      // console.log(data['rankings']);
-      // rankData.push(data['rankings']);
-      // console.log(rankData);
       }
       catch(err) {
         console.log(err);
@@ -144,13 +144,9 @@ function Picklists(props: any) {
         {
           match_num.push(parseInt(data[i]['match_number']));
           match_score.push(parseInt(data[i]['score']))
-
-          console.log(match_num);
-          console.log(match_score);
-          console.log(typeof match_num);
-          console.log(typeof match_score);
         }
         match = match_num;
+        score = match_score;
 
         avg_score = 0;
         match_score.forEach(score => {
@@ -161,12 +157,13 @@ function Picklists(props: any) {
         expandData = [];
         for(var i = 0; i < match_num.length; i++) {
           let newChildren = {
-            key: team_number + match_num[i],
+            key: team_number * 1000 + match_num[i],
             match_number: match_num[i],
             score: match_score[i],
           }
           expandData.push(newChildren);
         }
+        setDataDetail(expandData);
       }
       catch (err) {
         console.log(err);
@@ -174,8 +171,6 @@ function Picklists(props: any) {
     };
   }, [team_number]);
   
-  // console.log(rankData, '00');
-  // console.log(rankData[0]);
   return (
     <body>
       <div className='banner'>
@@ -191,7 +186,19 @@ function Picklists(props: any) {
           </table>
         </header>
         <h2 style={{whiteSpace: 'pre-line'}}>{loading ? 'Loading...' : ''}</h2>
-        <Table columns={columns} dataSource={fetchedData} pagination={tableParams.pagination}></Table>
+        <Table 
+          columns={columns} 
+          dataSource={fetchedData} 
+          pagination={tableParams.pagination}
+          expandable={{
+            expandedRowRender: OwO => (
+              <Table
+                columns={expandColumns}
+                dataSource={dataDetail}
+              />
+            ),
+          }}
+          ></Table>
       </div>
     </body>
   );
