1069a5181e10639ef58a5275803fa3277303adbf
LisaChenn <98070000+LisaChenn@users.noreply.github.com>
2024-02-25 11:29:50 -0800

    color change

--- integrated-scouting-apps/src/routes/scoutingapp.tsx
+++ integrated-scouting-apps/src/routes/scoutingapp.tsx
@@ -22,13 +22,13 @@ function ScoutingPage(props: any) {
         </header>
       </div>
       <div>
-        <Button className='mainbutton' href='/scoutingapp/match'>Match</Button>
+        <Button style={{  paddingBottom: '15px'   }} className='mainbutton' href='/scoutingapp/match'>Match</Button>
         <Button className='mainbutton' href='/scoutingapp/strategic'>Strategic</Button>
         <Button className='mainbutton' href='/scoutingapp/pit'>Pit</Button>
         <Button className='mainbutton' href='/scoutingapp/lookup'>Data Lookup</Button>
         <Button className='mainbutton' href='/scoutingapp/picklists'>Picklists</Button>
       </div>
-      <iframe height="500px" width="500px" src="https://vclock.com/embed/timer/#date=2024-02-28&title=finish+scouting+app+and+enjoy+life&theme=1" title='lisa gay'></iframe>
+      {/* <iframe height="500px" width="500px" src="https://vclock.com/embed/timer/#date=2024-02-28&title=finish+scouting+app+and+enjoy+life&theme=1"></iframe> */}
     </body>
   );
 }
