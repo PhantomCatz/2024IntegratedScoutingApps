68c45222508b897d6ee2ff00997c6b09b2ba1cc7
LisaChenn <98070000+LisaChenn@users.noreply.github.com>
2024-03-02 12:51:14 -0800

    trying ot fix

--- integrated-scouting-apps/src/routes/dtf.tsx
+++ integrated-scouting-apps/src/routes/dtf.tsx
@@ -1,55 +1,55 @@
-import '../public/stylesheets/dtf.css';
-import logo from '../public/images/logo.png';
-import back from '../public/images/back.png';
-import { useEffect } from 'react';
-import { Input, Form, InputNumber } from 'antd';
+// import '../public/stylesheets/dtf.css';
+// import logo from '../public/images/logo.png';
+// import back from '../public/images/back.png';
+// import { useEffect } from 'react';
+// import { Input, Form, InputNumber } from 'antd';
 
 function DTF(props: any) {
-	const [form] = Form.useForm();
-	useEffect(() => document.title = props.title, [props.title]);
+// 	const [form] = Form.useForm();
+// 	useEffect(() => document.title = props.title, [props.title]);
 
-	return (
-		<body>
-			<div className='banner'>
-				<header>
-					<a href='/home'>
-						<img src={back} style={{ height: 64 + 'px', paddingTop: '5%' }} alt=''></img>
-					</a>
-					<table>
-						<td>
-							<img src={logo} style={{ height: 256 + 'px' }} alt=''></img>
-						</td>
-						<td>
-							<h1 style={{ display: 'inline-block', textAlign: 'center' }}>Drive Team Feeder</h1>
-						</td>
-					</table>
-				</header>
-			</div>
-			<Form
-				form={form}
-				onFinish={async event => {
-          			const teamNums = [event.team1Num, event.team2Num, event.team3Num].filter(num => num !== undefined);
-          			window.location.href = "/dtf/" + teamNums.join(",");
-				}}
-			>
-				<div>
-					<h2>Team 1 Number</h2>
-						<Form.Item name="team1Num" rules={[{ required: true, message: "Please input the team number!"}]}>
-							<InputNumber min={0} className="input" />
-						</Form.Item>
-          			<h2>Team 2 Number</h2>
-          				<Form.Item name="team2Num">
-							<InputNumber min={0} className="input" />
-						</Form.Item>
-          			<h2>Team 3 Number</h2>
-          				<Form.Item name="team3Num">
-							<InputNumber min={0} className="input" />
-						</Form.Item>
-					<Input type="submit" value="Submit" className='submit'/>
-				</div>
-			</Form>
-		</body>
-	);
-}
+// 	return (
+// 		<body>
+// 			<div className='banner'>
+// 				<header>
+// 					<a href='/home'>
+// 						<img src={back} style={{ height: 64 + 'px', paddingTop: '5%' }} alt=''></img>
+// 					</a>
+// 					<table>
+// 						<td>
+// 							<img src={logo} style={{ height: 256 + 'px' }} alt=''></img>
+// 						</td>
+// 						<td>
+// 							<h1 style={{ display: 'inline-block', textAlign: 'center' }}>Drive Team Feeder</h1>
+// 						</td>
+// 					</table>
+// 				</header>
+// 			</div>
+// 			<Form
+// 				form={form}
+// 				onFinish={async event => {
+//           			const teamNums = [event.team1Num, event.team2Num, event.team3Num].filter(num => num !== undefined);
+//           			window.location.href = "/dtf/" + teamNums.join(",");
+// 				}}
+// 			>
+// 				<div>
+// 					<h2>Team 1 Number</h2>
+// 						<Form.Item name="team1Num" rules={[{ required: true, message: "Please input the team number!"}]}>
+// 							<InputNumber min={0} className="input" />
+// 						</Form.Item>
+//           			<h2>Team 2 Number</h2>
+//           				<Form.Item name="team2Num">
+// 							<InputNumber min={0} className="input" />
+// 						</Form.Item>
+//           			<h2>Team 3 Number</h2>
+//           				<Form.Item name="team3Num">
+// 							<InputNumber min={0} className="input" />
+// 						</Form.Item>
+// 					<Input type="submit" value="Submit" className='submit'/>
+// 				</div>
+// 			</Form>
+// 		</body>
+// 	);
+ }
 
 export default DTF;
